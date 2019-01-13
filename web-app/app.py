5d01789f7783ae8b6bd7806090ca8d0d5f193a6b
nimasratna <nimasratn@gmail.com>
2019-01-13 21:20:30 +0100

    change to SQLite3

--- web-app/app.py
+++ web-app/app.py
@@ -1,45 +1,58 @@
 from flask import Flask, jsonify, request, make_response, session, render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 import requests
 import json
 import os
-
-import chaincodes
+import sqlite3
+from chaincodes import *
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.config['SECRET_KEY'] = 'thisissecret'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:rootsuper@localhost:3306/e-recepie' # hosted MySQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')  # NoSQL - SQLite
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')  # NoSQL - SQLite
 
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
+conn = sqlite3.connect('e-recipe.db')
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS user (idUser REAL, FirstName TEXT, LastName TEXT, Role REAL, email TEXT, insurance TEXT, insurance_starting_date TEXT, insurance_expired_date TEXT, birthday TEXT, password TEXT)")
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # username = db.Column(db.String(45))
-    FirstName = db.Column(db.String(45))
-    LastName = db.Column(db.String(45))
-    Role = db.Column(db.String(45))
-    insurance = db.Column(db.String(45))
-    insurance_startdate = db.Column(db.Date)
-    insurance_expireddate = db.Column(db.Date)
-    password = db.Column(db.String(255))
-    email = db.Column(db.String(255))
-    birthday = db.Column(db.Date)
-
-
-db.create_all()
-db.session.commit()
-
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     # username = db.Column(db.String(45))
+#     FirstName = db.Column(db.String(45))
+#     LastName = db.Column(db.String(45))
+#     Role = db.Column(db.String(45))
+#     insurance = db.Column(db.String(45))
+#     insurance_startdate = db.Column(db.Date)
+#     insurance_expireddate = db.Column(db.Date)
+#     password = db.Column(db.String(255))
+#     email = db.Column(db.String(255))
+#     birthday = db.Column(db.Date)
+#
+#
+# db.create_all()
+# db.session.commit()
+#
 
 # For id auto-numbering
 TRANSACTION_ID = 10
 RECIPE_ID = 10
+
+
+def read_id():
+    conn1 = sqlite3.connect('e-recipe.db')
+    c1 = conn1.cursor()
+    c1.execute('SELECT count(idUser) from user')
+    data =c1.fetchone()
+    id = int(data[0])+1
+    c1.close()
+    conn1.close()
+    return id
 
 
 @app.route('/isloggedin', methods=['GET'])
@@ -59,33 +72,34 @@ def create_user():
     FirstName = postedData["FirstName"]
     LastName = postedData["LastName"]
     Role = postedData["Role"]
-    insurance = postedData["insurance"]
-    insurance_start = datetime.strptime(postedData["insurancestart"], '%Y-%m-%d')
-    insurance_startdate = insurance_start.date()
-    insurance_expired = datetime.strptime(postedData["insuranceexpired"], '%Y-%m-%d')
-    insurance_expireddate = insurance_expired.date()
     password = postedData["password"]
     email = postedData["email"]
     birthday_datetime = datetime.strptime(postedData["birthday"], '%Y-%m-%d')
     birthday = birthday_datetime.date()
-    #print(birthday)
+    print(birthday)
 
     if '' in postedData.values():
         return jsonify({'success': False, 'message': 'All fields must be filled'})
 
     # i dont check password and its repeated in here because it can be easily check in frontend without need to send data to backend anymore
 
-    user = User.query.filter_by(email=email).first()
-    if user:
+    #user = User.query.filter_by(email=email).first()
+    conn = sqlite3.connect('e-recipe.db')
+    c = conn.cursor()
+    c.execute("SELECT idUser FROM user WHERE email=(?)", (email,))
+    user = c.fetchone()
+    if user is not None:
         return jsonify({'success': False, 'message' : 'user already exists'})
 
     hashed_password = generate_password_hash(password, method='sha256')
 
-    new_user = User(FirstName=FirstName, LastName=LastName, Role = Role,insurance = insurance, insurance_startdate = insurance_startdate,insurance_expireddate = insurance_expireddate, password=hashed_password, email=email, birthday=birthday)
-    db.session.add(new_user)
-    db.session.commit()
-    db.session.commit()
-
+    id = read_id()
+    c.execute(
+        "INSERT INTO user (idUser, FirstName, LastName, Role, password, email, birthday) VALUES ( ?, ?, ?, ?, ?, ?, ?)",
+        (id, FirstName, LastName, Role, hashed_password, email, birthday))
+    conn.commit()
+    c.close()
+    conn.close()
     return jsonify({'success': True, 'message' : 'New user created!'})
 
 
@@ -95,23 +109,32 @@ def login():
     usermail = postedData["email"]
     password = postedData["password"]
 
+    conn = sqlite3.connect('e-recipe.db')
+    c = conn.cursor()
+
     if not postedData or not usermail or not password:
         return jsonify({'success': False, 'message' : 'Please enter both email and password'})
 
-    user = User.query.filter_by(email=usermail).first()
+    #user = User.query.filter_by(email=usermail).first()
+    #user = []
+    c.execute("SELECT idUser, FirstName, LastName, Role, password, insurance FROM user WHERE email = (?)", (usermail,))
+    data= c.fetchone()
 
-    if not user:
+    if data[0] is None:
         return jsonify({'message' : 'wrong credentials'})
 
-    if check_password_hash(user.password, password):
+    if check_password_hash(data[4], password):
         session['logged'] = True
-        session['role'] = user.Role
-        session['id'] = user.id
-        session['FirstName'] = user.FirstName
-        session['LastName'] = user.LastName
-        session['insurance'] = user.insurance
+        session['role'] = data[3]
+        session['id'] = data[0]
+        session['FirstName'] = data[1]
+        session['LastName'] = data[2]
+        session['insurance'] = data[5]
         return jsonify({'success': True, 'message' : 'login successful'})
-    return jsonify({'success': False, 'message' : 'wrong credentials'})
+    else:
+        return jsonify({'success': False, 'message' : 'wrong credentials!!'})
+    c.close()
+    conn.close()
 
 
 @app.route('/logout', methods=['GET'])
@@ -136,7 +159,7 @@ def index():
             session.clear()
 
 @app.route('/chemist/add_transaction', methods=['POST'])
-def chemist_add_transaction():
+def add_transaction():
     data = request.get_json()
     # TODO: Check if data is valid?
 
@@ -154,7 +177,7 @@ def chemist_add_transaction():
         abort(400)  # Bad request
 
 @app.route('/doctor/add_recipe', methods=['POST'])
-def doctor_add_recipe():
+def add_recipe():
     data = request.get_json()
      # TODO: Check if data is valid?
 
@@ -173,7 +196,7 @@ def doctor_add_recipe():
         abort(400)  # Bad request
 
 @app.route('/chemist/get_recipe/<id>', methods=['GET'])
-def chemist_get_recipe(id):
+def get_recipe(id):
     response = chaincodes.get_recipe_by_patient(id)
     if response is not None:
         return response
@@ -181,31 +204,20 @@ def chemist_get_recipe(id):
         abort(404)  # Not found
 
 @app.route('/doctor/get_patient/<id>', methods=['GET'])
-def doctor_get_patient(id):
-    patient = User.query.filter_by(id=id).first()
-    name = patient.FirstName + " " + patient.LastName
-    birthday = patient.birthday
-    insurance = patient.insurance
+def get_patient(id):
+    conn = sqlite3.connect('e-recipe.db')
+    c = conn.cursor()
+    c.execute("SELECT FirstName, LastName, birthday, insurance FROM user WHERE idUser = (?)", (id))
+    patient = c.fetchone()
+    #patient = User.query.filter_by(id=id).first()
+    name = patient[0]+ " " + patient[1]
+    birthday = patient[2]
+    insurance = patient[3]
+    c.close()
+    conn.close()
     return jsonify({'name': name, 'birthday': birthday, 'insurance': insurance})
 
 
-@app.route('/patient/get_recipes', methods=['GET'])
-def patient_get_recipes():
-	id = session['id'] 
-	response = chaincodes.get_recipe_by_patient(id)
-	if response is not None:
-		return response
-	else:
-		abort(404)  # Not found
-
-@app.route('/patient/get_transactions', methods=['GET'])
-def patient_get_transactions():
-	id = session['id'] 
-	response = chaincodes.get_transaction_by_patient(id)
-	if response is not None:
-		return response
-	else:
-		abort(404)  # Not found
 
 
 if __name__ == '__main__':
